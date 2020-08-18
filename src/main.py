20443f373806fa2c400494b3387001519ae5a3f6
AnabelSalomone <anabel.salomone@gmail.com>
2020-08-18 22:30:37 +0200

    add excercise

--- src/main.py
+++ src/main.py
@@ -65,3 +65,18 @@ def extra_end(str):
   return end * 3
 
 
+# The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes.
+# If either of you is very stylish, 8 or more, then the result is 2 (yes).
+# With the exception that if either of you has style of 2 or less, then the result is 0 (no).
+# Otherwise the result is 1 (maybe).
+
+def date_fashion(you, date):
+  if (you > 7 and not date < 3) or (date > 7 and not you < 3):
+    return 2
+
+  if you < 3 or date < 3:
+    return 0
+
+  return 1
+
+
